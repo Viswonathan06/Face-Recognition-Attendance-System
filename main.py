c4f3ced633198e3eb4d1f05eef7b34147f0a108b
Viswonathan <viswonathan0606@gmail.com>
2021-01-13 01:05:09 +0400

    Basic attendance functionality added.
    Need to implement code for adding pictures to database. And implement GUI method of using the app.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import dlib
 import cmake
 import face_recognition
 import os
+from datetime import datetime
 
 
 path = "Face Recog pics"
@@ -12,6 +13,28 @@ classNames = []
 myList = os.listdir(path)
 print(myList)
 
+
+
+############################################## Functions ################################################
+
+## Attendance marking function
+def markAttendance( name ):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')     ## As the file is csv format
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            # print(now)
+            # print(now.strftime())
+            ## Implement a column for if the person is late or not.
+            Date = now.strftime('%D')
+            dtString = now.strftime('%H : %M : %S')
+            f.writelines(f'\n{name},{dtString},{Date}')
+
+## Encoding all the pictures present in the path
 for clas in myList:
     curImg = cv.imread(f'{path}/{clas}')
     images.append(curImg)
@@ -28,34 +51,48 @@ def encodeImages( images ):
         # print(face_recognition.face_encodings(img))
     return encodings
 
+
+
+
+
+##############################################################################################################3
+
+
 encodeExisting = encodeImages( images )
+##printing length for verification
 print(len(encodeExisting))
 print("Completed encoding pictures")
-
-
+## Start camera
 camera = cv.VideoCapture(0)
 while True:
     success, img = camera.read()
     img = cv.flip(img, 1)
+    ## Resizing the images to 1/4th size and converting them from BGR to RGB
     imgResized = cv.resize(img, (0,0), None, 0.25, 0.25)
     imgResized = cv.cvtColor(imgResized, cv.COLOR_BGR2RGB)
-    
+
+    ## Finding face locations on the resizedd images and encoding them based on their location
     curr_faceLocation = face_recognition.face_locations(imgResized)
     curr_facesEncoded = face_recognition.face_encodings(imgResized, curr_faceLocation)
 
+    ## Looping through the encodings and locations and comparing the faces
     for encodeFace,faceLocation in zip(curr_facesEncoded, curr_faceLocation):
         matches = face_recognition.compare_faces( encodeExisting, encodeFace)
         faceDist = face_recognition.face_distance(  encodeExisting, encodeFace )
         print(faceDist)
+        ## Finding the minimum distance between two faces
         matchIndex = np.argmin( faceDist )
-
+        ## If match occurs, draw bounding box and display name.
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
             print(name)
-            cv.rectangle(img, (faceLocation[3], faceLocation[0]),
-                         (faceLocation[1], faceLocation[2]), (255, 255, 0))
+            y1, x1, y2, x2 = faceLocation
+            y1, x1, y2, x2 = y1*4, x1*4, y2*4, x2*4
+            cv.rectangle(img, (x1, y1),
+                         (x2, y2), (255, 255, 0))
             cv.putText(img, f'{name} ', (50,50), cv.FONT_HERSHEY_TRIPLEX, 1, (0, 255, 255), 2)
-
+            markAttendance(name)
+    ## Show frame
     cv.imshow("Frame", img)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
